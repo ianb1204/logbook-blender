17cd27ad1db7f3bff32db03cbddf74c5bac57550
Ian BERTIN <ian.bertin2004@gmail.com>
2026-03-05 14:47:17 +0100

    Page 404 pour redirection Github Pages

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,11 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom'
 import App from "./pages/App.jsx";
 
 const Router = (props) => {
     return (
         <BrowserRouter basename='/logbook-blender/'>
             <Routes>
-                <Route path={'/*'} element={<App {...props}/>}/>
+                <Route path={'/*'} element={<Navigate to={"/fr"}/>}/>
                 <Route path={'/:lang'} element={<App {...props}/>}/>
             </Routes>
         </BrowserRouter>
